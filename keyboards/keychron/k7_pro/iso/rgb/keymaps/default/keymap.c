5433b52f815a5837016c5e3f5b1a4360d0fcca2e
Thomas Dobber <thomasdobber@gmail.com>
2025-02-01 16:46:06 +0100

    lt change

--- keyboards/keychron/k7_pro/iso/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k7_pro/iso/rgb/keymaps/default/keymap.c
@@ -30,6 +30,14 @@ enum layers{
 /**
  * Combos
  */
+ enum myCombos {
+    A,
+    B,
+    C,
+    D,
+    COMBO_LENGTH
+};
+uint16_t COMBO_LEN = COMBO_LENGTH;
 const uint16_t PROGMEM c_df[] = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM c_sdf[] = {KC_S, KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM c_jk[] = {KC_J, KC_K, COMBO_END};
@@ -39,7 +47,7 @@ combo_t key_combos[] = {
     [A] = COMBO(c_sdf, KC_ESC),
     [B] = COMBO(c_df, KC_TAB),
     [C] = COMBO(c_jk, KC_BSPC),
-    [D] = COMBO(c_jkl, KC_ENT),
+    [D] = COMBO(c_jkl, KC_ENT)
 };
 
 
@@ -49,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                      KC_HOME,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,             KC_PGUP,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_PGDN,
-        KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,MO(MAC_FN1),MO(FN2), KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LOPTN, KC_LCMMD,                               LT_SPC,                                 KC_RCMMD,MO(MAC_FN1),MO(FN2), KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_iso(
         KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_DEL,
